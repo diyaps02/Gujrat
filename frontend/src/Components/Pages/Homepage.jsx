583b3279e9cb6472b9e86809b00644b64878214d
Diya Singhal <adcomdiya@gmail.com>
2024-09-11 17:47:05 +0530

    update files

--- frontend/src/Components/Pages/Homepage.jsx
+++ frontend/src/Components/Pages/Homepage.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { BsBuildingsFill } from "react-icons/bs";
-
+import Footer from "../common/Footer";
+import Aboutus from "../Pages/About";
+import Latestnews from "../../Components/Pages/Latestnews";
+import About from '../Pages/About';
 const Homepage = () => {
   return (
-    <div className='bg-homepage  w-full bg-cover pt-11  h-screen'>
+   <div>
+
+    {/* homepage start */}
+     <div className='bg-homepage  w-full bg-cover pt-11  h-screen'>
    <div className='flex flex-col justify-center '>
     <div className='bg-blue-500 rounded-md h-80 w-6/12 opacity-85 mt-16 ml-16'>
             ahrfjh
     </div>
   <div className='flex justify-center'>
-  <div className='bg-black mt-16 w-9/12 h-40 opacity-75 flex gap-10 px-16 rounded-md shadow-black shadow-md text-white p-2'>
+  <div className='bg-black mt-16 w-8/12 h-40 opacity-75 flex gap-10 px-16 rounded-md shadow-black shadow-md text-white p-2'>
         <div className='flex items-center gap-4'>
         <BsBuildingsFill  className='text-4xl'/>
        <div className='flex flex-col'>
@@ -38,17 +44,15 @@ const Homepage = () => {
        <h1 className='font-bold'>PATENTS</h1>
        </div>
         </div>
-        <div className='flex items-center gap-4'>
-        <BsBuildingsFill  className='text-4xl'/>
-       <div className='flex flex-col'>
-       <h1  className='text-4xl font-bold'>1000+</h1>
-       <h1 className='font-bold'>PATENTS</h1>
-       </div>
-        </div>
     </div>
   </div>
    </div>
     </div>
+    {/* homepage end */}
+    <About/>
+    <Latestnews/>
+    <Footer/>
+   </div>
   )
 }
 
--- frontend/src/Components/Pages/Latestnews.jsx
+++ frontend/src/Components/Pages/Latestnews.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import scheme from "../../Assets/Images/schemes.jpg"
+import scheme3 from "../../Assets/Images/scheme3.jpg";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Latestnews = () => {
 
   const option=["All","News","Anouncement","Reports"];
@@ -13,15 +16,16 @@ const Latestnews = () => {
       <div className='w-full flex mx-auto px-12 gap-8 h-5/6 pt-6'>
         {/* written news section */}
         <div className='w-5/12  flex justify-center items-center'>
-          <div className='flex flex-col rounded-md shadow-lg shadow-grey-700 pb-4 mx-auto w-9/12 '>
-            <div className='flex gap-2 bg-blue text-white mx-auto py-4'>
+          <div className='flex flex-col rounded-md shadow-lg shadow-grey-700 pb-4 mx-auto w-9/12 h-full '>
+
+            <div className='flex gap-2 bg-blue text-white mx-auto py-4 '>
             {
             option.map((ele,index)=>(
               <button key={index} className='text-xl p-2 px-3 bg-blue-500 text-white rounded-md'>{ele}</button>
             ))
           }
             </div>
-            <div className='h-[100%] flex flex-col gap-2'>
+            <div className=' flex flex-col gap-2 h-[calc(100%-60px)] overflow-y-auto'>
 
              <div className='flex gap-4'>
               <i>-</i>
@@ -30,6 +34,7 @@ const Latestnews = () => {
                 Jan 17 ,2025
 
               </div>
+              
              </div>
              <div className='flex gap-4'>
               <i>-</i>
@@ -72,13 +77,58 @@ const Latestnews = () => {
 
               </div>
              </div>
+             <div className='flex gap-4'>
+              <i>-</i>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, odio?<br/>
+                Jan 17 ,2025
+
+              </div>
+             </div>
+             <div className='flex gap-4'>
+              <i>-</i>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, odio?<br/>
+                Jan 17 ,2025
+
+              </div>
+             </div>
+             <div className='flex gap-4'>
+              <i>-</i>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, odio?<br/>
+                Jan 17 ,2025
+
+              </div>
+             </div>
+             <div className='flex gap-4'>
+              <i>-</i>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, odio?<br/>
+                Jan 17 ,2025
+
+              </div>
+             </div>
 
             </div>
           </div>
         </div>
         {/* carausel section */}
-        <div className='w-7/12 bg-orange-200'>
-hello
+        <div className='w-7/12 bg-orange-200 flex items-center pt-16'>
+        <Carousel>
+            <div>
+              <img src={scheme3} alt="News 1" />
+              <p className="legend">News 1</p>
+            </div>
+            <div>
+              <img src={scheme} alt="News 2" />
+              <p className="legend">News 2</p>
+            </div>
+            <div>
+              <img src="image3.jpg" alt="News 3" />
+              <p className="legend">News 3</p>
+            </div>
+          </Carousel>
         </div>
       </div>
 
